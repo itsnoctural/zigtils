453be84926a958f58839502f4d55da67ae49e991
itsnoctural <85783226+itsnoctural@users.noreply.github.com>
2026-02-03 19:54:05 +0200

    cat: implement prototype

--- src/cat.zig
+++ src/cat.zig
@@ -6,18 +6,34 @@ const process = std.process;
 const version = "0.0.1";
 
 const Options = struct {
-    number_lines: bool,
-    show_ends: bool,
+    number_lines: bool = false,
+    show_ends: bool = false,
 };
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const allocator = init.gpa;
-    const args = parseArguments(init.minimal.args);
 
-    _ = io;
-    _ = allocator;
-    _ = args;
+    var options = Options{};
+    var iterator = init.minimal.args.iterate();
+
+    while (iterator.next()) |v| {
+        if (std.mem.eql(u8, v, "-n") or std.mem.eql(u8, v, "--number")) {
+            options.number_lines = true;
+        } else if (std.mem.eql(u8, v, "-E") or std.mem.eql(u8, v, "--show-ends")) {
+            options.show_ends = true;
+        }
+    }
+
+    const cwd = Io.Dir.cwd();
+    const stat = try cwd.statFile(io, "test/foo.c", .{});
+
+    const buff = try allocator.alloc(u8, stat.size);
+    defer allocator.free(buff);
+
+    const content = try cwd.readFile(io, "test/foo.c", buff);
+
+    std.debug.print("content: {s}\nsize: {any}\n", .{ content, stat.size });
+    std.debug.print("{}, {}\n", .{ options.number_lines, options.show_ends });
+    _, _ = .{ io, allocator };
 }
-
-fn parseArguments(args: process.Args) void {}
